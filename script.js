520b7ffee578d604efbea3b7ab2131f5b88d7dec
Elton Vieira <technology.elton@gmail.com>
2022-05-29 13:49:51 -0300

    .

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const validateInput = () => inputElement.value.trim().length > 0;
 const handleAddTask = () => {
     const inputIsValid = validateInput();
     if(!inputIsValid) {
-        return alert('Informe o Nome do Aluno!!')
+        return alert('❌ Informe o Nome do Aluno!!')
     }
     const taskItemContainer = document.createElement('div');
     taskItemContainer.classList.add('task-item');
